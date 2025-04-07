2c98fd9f3eb94efb68453b548e871ce725a4df56
Hatice AKGÜL <katrancihatice@icloud.com>
2025-04-07 22:42:20 +0300

    Clean commands line

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'; // ✔️ bu doğru
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import Jobs from './pages/Jobs';
